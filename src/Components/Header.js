6a30797ab5bfe48e6676623d9958430468dd23b2
jayvon <jaheyliger@yahoo.com>
2021-03-05 10:50:52 -0500

    initial upload

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,7 +17,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-      <ParticlesBg type="circle" bg={true} />
+      <ParticlesBg type="polygon" bg={true} />
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -32,12 +32,13 @@ class Header extends Component {
       </nav>
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline"><Typed strings={['Hello', 'My Name Is', `${name}`]} typeSpeed={65} /></h1>
+            <h1 className="responsive-headline">{name}</h1>
+            {/* <h1 className="responsive-headline"><Typed strings={['Hello', 'My Name Is', `${name}`]} typeSpeed={65} /></h1> */}
             <h3>{description}</h3>
             <hr />
             <ul className="social">
-               <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
-               <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
+               <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Projects Button?</a>
+               <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github Button?</a>
             </ul>
          </div>
       </div>
